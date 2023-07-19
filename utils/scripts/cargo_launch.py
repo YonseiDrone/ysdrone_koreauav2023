3a28b4bd7ce9c5b14c53b85e0e3d8e8e7b8b38c5
ChanJoon <as06047@yonsei.ac.kr>
2023-07-19 14:05:51 +0900

    Update cargo launching

--- utils/scripts/cargo_launch.py
+++ utils/scripts/cargo_launch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from mavros_msgs.srv import ParamSet, ParamGet
+from mavros_msgs.srv import ParamSet, ParamGet, CommandLong
 from mavros_msgs.msg import ParamValue
 
 def get_mavros_param():
@@ -30,10 +30,12 @@ def set_mavros_param():
 
     # Wait for the service to become available
     rospy.wait_for_service('/mavros/param/set')
-
+    rospy.wait_for_service('/mavros/cmd/command')
+    
     try:
         # Create a handle to the service
         param_set_service = rospy.ServiceProxy('/mavros/param/set', ParamSet)
+        servo_control_srv = rospy.ServiceProxy('/mavros/cmd/command', CommandLong)
 
         # Create the parameter value
         param_value = ParamValue()
@@ -41,12 +43,23 @@ def set_mavros_param():
         param_value.real = 0.0
 
         # Call the service
-        resp = param_set_service(param_id='RC_MAP_AUX1', value=param_value)
-        if resp.success:
+        resp_set = param_set_service(param_id='RC_MAP_AUX1', value=param_value)
+        if resp_set.success:
             print("Parameter set successfully")
         else:
             print("Failed to set parameter")
         get_mavros_param()
+        
+    	try:
+        resp = servo_control_service(False, 187, 94, 1, 1500, 0, 0, 0, 0, 0)
+        
+		if resp.success:
+		    print("Servo controlled successfully")
+		else:
+		    print("Failed to control servo")
+
+    	except rospy.ServiceException as e:
+		print("Service call failed: %s" % e)
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
